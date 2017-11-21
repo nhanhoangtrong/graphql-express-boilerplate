6f7dae7dec7f6b0d7d0e380ec1779e2f89a1c01d
Nhan Hoang <siege250294@gmail.com>
2017-11-21 19:22:59 +0700

    knex DEBUG configuration

--- src/knex.js
+++ src/knex.js
@@ -7,20 +7,16 @@ const db = knex({
     migrations: {
         tableName: process.env.PG_MIGRATIONS_TABLE || 'migrations',
     },
-    debug: process.env.NODE_ENV === 'development',
+    debug: process.env.DEBUG === 'true',
 });
 
 // Debug queries in KnexJS
 db.on('query', (query) => {
-    logger.debug(`Execute a query id: ${query.__knexQueryUid }`);
+    logger.debug(`Query id: ${query.__knexQueryUid}`);
+    logger.debug(`Query: ${query.sql}`);
 }).on('query-response', (response, query) => {
     logger.debug(`Received a response from: ${query.__knexQueryUid}`);
-    logger.debug(`SQL query: ${query.sql}`);
     logger.debug(`Response: \n${JSON.stringify(response, null, 2)}`);
-}).on('query-error', (err, query) => {
-    logger.debug(`Query id: ${query.__knex.__knexQueryUid} has an error: ${err.name}`);
 });
-
-logger.info('Successfully connect to PostgreSQL');
 
 export default db;
