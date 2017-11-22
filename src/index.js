de2d74f935ea711f5b93545f07a96a1e908e8a08
Nhan Hoang <siege250294@gmail.com>
2017-11-22 20:20:57 +0700

    fixing connect mongoose error on testing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import './loadenv';
 import { createServer } from 'http';
-import app from './app';
+import { connectMongoose } from './mongoose';
 import logger from './logger';
+import app from './app';
 
-logger.info(`Starting server on ${process.env.NODE_ENV} mode`);
-const server = createServer(app);
-server.listen(app.get('port'), app.get('host'));
-server.on('listening', () => {
-    logger.info(`Server is listening on http://${app.get('host')}:${app.get('port')}/`);
+connectMongoose().then(() => {
+    logger.info(`Starting server on ${process.env.NODE_ENV} mode`);
+    const server = createServer(app);
+    server.listen(app.get('port'), app.get('host'));
+    server.on('listening', () => {
+        logger.info(`Server is listening on http://${app.get('host')}:${app.get('port')}/`);
+    });
+    server.on('error', (err) => {
+        logger.error(`Starting Server Error: ${err.name}`);
+        logger.error(`Message: ${err.message}`);
+        logger.error(`Stack: ${err.stack}`);
+        process.exit(1);
+    });
+    server.on('close', () => {
+        logger.info(`Server is going to close`);
+    });
 });
-server.on('error', (err) => {
-    logger.error(`Starting Server Error: ${err.name}`);
-    logger.error(`Message: ${err.message}`);
-    logger.error(`Stack: ${err.stack}`);
-    process.exit(1);
-});
-server.on('close', () => {
-    logger.info(`Server is going to close`);
-});
-
-export default server;
